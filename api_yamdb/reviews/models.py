2907a7e4438c00117a43d807e57e07546a733e72
Galt12 <qvaqsa@gmail.com>
2022-09-24 21:51:06 +0300

    2_models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Title(models.Model):
+    category =models.ForeignKey(
+        Categorie,
+        on_delete=models.CASCADE,
+        null=True,
+        black=True
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE
+    )
+    name = models.TextField()
+    year = models.IntegerField()
+    description = models.TextField(
+        null=True,
+        verbose_name='Описание'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self) -> str:
+        return self.name
+
