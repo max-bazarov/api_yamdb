e8d053a4286baaa130d4fe11e722632e428fcedd
Alex J <onix8@mail.ru>
2022-09-26 17:09:30 +0500

    api/views.py edited

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,79 @@
+from django.core.mail import EmailMessage
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import permissions, status, viewsets
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from api_yamdb.api.serializers import CategorySerializer, GenreSerializer
 from api_yamdb.reviews.models import Category, Genre
+
+
+class APIGetToken(APIView):
+    """
+    Получение JWT-токена в обмен на username и confirmation code.
+    Права доступа: Доступно без токена. Пример тела запроса:
+    {
+        "username": "string",
+        "confirmation_code": "string"
+    }
+    """
+    def post(self, request):
+        serializer = GetTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        try:
+            user = User.objects.get(username=data['username'])
+        except User.DoesNotExist:
+            return Response(
+                {'username': 'Пользователь не найден!'},
+                status=status.HTTP_404_NOT_FOUND)
+        if data.get('confirmation_code') == user.confirmation_code:
+            token = RefreshToken.for_user(user).access_token
+            return Response({'token': str(token)},
+                            status=status.HTTP_201_CREATED)
+        return Response(
+            {'confirmation_code': 'Неверный код подтверждения!'},
+            status=status.HTTP_400_BAD_REQUEST)
+
+
+class APISignup(APIView):
+    """
+    Получить код подтверждения на переданный email. Права доступа: Доступно без
+    токена. Пример тела запроса:
+    {
+        "email": "string",
+        "username": "string"
+    }
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    @staticmethod
+    def send_email(data):
+        email = EmailMessage(
+            subject=data['email_subject'],
+            body=data['email_body'],
+            to=[data['to_email']]
+        )
+        email.send()
+
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        email_body = (
+            f'Здравствуйте, {user.username}.'
+            f'\nКод подтверждения для доступа к API: {user.confirmation_code}'
+        )
+        data = {
+            'email_body': email_body,
+            'to_email': user.email,
+            'email_subject': 'Код подтверждения для доступа к API!'
+        }
+        self.send_email(data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 
 class GenresViewSet(viewsets.ModelViewSet):
