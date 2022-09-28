96aa1b2a59b70f2fa7b0296e8f1f1bec6e80fc8a
Max Bazarov <bazaroffm@gmail.com>
2022-09-28 17:56:23 +0300

    upgrade user model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+ROLES = (
+    ('user', 'user'),
+    ('moderator', 'moderator'),
+    ('admin', 'admin'),
+)
 
 
 class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    role = models.CharField(
+        max_length=20,
+        choices=ROLES,
+        default='user',
+        verbose_name='Роль'
+    )
+
     def __str__(self):
         return self.username
