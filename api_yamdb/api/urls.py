2ba385f4a17a7298df5bc95ad112f75a3d79fd4c
Max Bazarov <bazaroffm@gmail.com>
2022-09-29 11:59:46 +0300

    add review endpoint

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,19 +1,18 @@
+from api.views import (APIGetToken, APISignup, CategoryViewSet, CommentViewSet,
+                       GenreViewSet, ReviewViewSet, TitleViewSet)
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                       ReviewViewSet, TitleViewSet, APIGetToken, APISignup)
-
 router = DefaultRouter()
 router.register('genres', GenreViewSet, basename='genre')
-router.register('title', TitleViewSet, basename='title')
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet, basename='comment'
-)
+router.register('titles', TitleViewSet, basename='title')
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet, basename='review'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comment'
 )
 router.register('categories', CategoryViewSet, basename='category')
 
