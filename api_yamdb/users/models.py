3a7932b21b60fcf6a6521ff5d9b447f92a6ba270
Max Bazarov <bazaroffm@gmail.com>
2022-09-30 11:25:59 +0300

    add email field

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -16,6 +16,11 @@ class User(AbstractUser):
         default='user',
         verbose_name='Роль'
     )
+    confirmation_code = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(
+        max_length=255,
+        unique=True
+    )
 
     def __str__(self):
         return self.username
