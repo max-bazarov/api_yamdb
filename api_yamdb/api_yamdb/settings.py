b9340831ad677e57351420e3745df82c26eda7a6
Max Bazarov <bazaroffm@gmail.com>
2022-10-02 00:53:08 +0300

    fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'users',
     'reviews',
     'api',
